f8649049a5a990e525f25f992ec4c39f27a78b00
11SF <nawapong.sit@gmail.com>
2023-06-13 00:17:19 +0700

    allow cors

--- server.go
+++ server.go
@@ -4,7 +4,6 @@ import (
 	"go_log_manager/router"
 
 	"github.com/joho/godotenv"
-	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
@@ -16,10 +15,11 @@ func main() {
 	e := router.New()
 
 	e.Use(middleware.CORS())
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"https://family-pay-14de8.web.app", "http://localhost:3000", "https://familypay.11sf.site"},
-		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
-	}))
+	// e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+	// 	AllowOrigins: []string{"https://family-pay-14de8.web.app", "http://localhost:3000", "https://familypay.11sf.site"},
+	// 	AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	// }))
 
+	e.Use(middleware.CORS())
 	e.Logger.Fatal(e.Start(":8080"))
 }
